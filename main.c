305c37c9296cabeb899ccf774d052a0ec393f580
Retana Romero Ivan Ascary <ianyretans@gmail.com>
2025-02-27 10:56:08 -0600

    practica realizada

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "conjunto.h"
+
+
+int main(){
+    CONJUNTO c, d, e;
+    c= conjunto_vacio();
+    d= conjunto_vacio();
+    e= conjunto_vacio();
+    print_conjunto(c);
+    print_conjunto(d);
+    print_conjunto(e);
+
+    printf("el conjunto c esta vacio %d\n", es_vacio(c));
+    c= agregar(c, 2);
+    c= agregar(c, 3);
+    c= agregar(c, 19);
+    c= agregar(c, 11);
+    c= agregar(c, 15);
+    print_conjunto(c);
+
+    d = agregar(d, 45);
+    d = agregar(d, 35);
+    d = agregar(d, 4);
+    d = agregar(d, 5);
+    print_conjunto(d);
+
+    e = _union(c, d);
+    print_conjunto(e);
+
+    return 0;
+
+}
